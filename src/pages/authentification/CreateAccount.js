f0581d7a269aa519b90a8c7736a7c8dd3f7c5ddd
HyenaSavva <re4meister@gmail.com>
2022-08-11 19:34:27 +0300

    Renamed components

---
+++ src/pages/authentification/CreateAccount.js
@@ -0,0 +1,69 @@
+import CustomInput from "./custom/CustomInput";
+import PasswordInput from '../../UI/inputs/PasswordInput'
+import { CustomButtonStyle } from "./custom/CustomStyles";
+
+import logo from "../../assets/logo-assist-tagline.svg";
+import { CustomButton } from "UI/button/CustomButton";
+import { GoogleButton } from "./custom/GoogleButton";
+
+import CreateAccountStyle from "./CreateAccountStyle";
+import { InputLabel } from "@mui/material";
+import { useState } from "react";
+
+const CreateAccount = ({
+  id,
+  label = { label1: "Email", label2: "Password" },
+  placeholder = {},
+}) => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const clickHandler = () => {
+    setIsLogin((state) => !state);
+  };
+
+  return (
+    <main className="Main">
+      <section className="LogBlock">
+        <form className="Log">
+          <img className="Img" src={logo} alt="desc" />
+
+          <h1 className="H1">Create account</h1>
+          <p className="P">Sing up for free and become member.</p>
+          <GoogleButton>Sing up with Google</GoogleButton>
+          <div className="orBlock">
+            <div className="Line"></div>
+            <p className="Por">OR</p>
+            <div className="Line" />
+          </div>
+          <CustomInput
+            label={label.label1}
+            id={label.label1}
+            placeholder={label.label1}
+            type={label.label1}
+          />
+          <PasswordInput
+            label={label.label2}
+            id={label.label2}
+            placeholder={label.label2}
+            type={label.label2}
+          />
+
+          <InputLabel>At least 8 characters and one number.</InputLabel>
+          <CustomButton sx={CustomButtonStyle} variant="contained">
+            Sing up
+          </CustomButton>
+          <p className="P switch">
+            Already have an accout ?
+            <span className="login" onClick={clickHandler}>
+              {isLogin ? "Sign Up" : "Log in"}
+            </span>
+          </p>
+        </form>
+      </section>
+      <div className="ImageBlock" />
+      <style jsx>{CreateAccountStyle}</style>
+    </main>
+  );
+};
+
+export default CreateAccount;
---
+++ src/pages/authentification/custom/CustomStyles.js
@@ -0,0 +1,43 @@
+export const LabelStyle = {
+  fontFamily: "Inter",
+  fontStyle: "normal",
+  fontWeight: "500",
+  fontSize: "14px",
+  lineHeight: "22px",
+  color: "#4D5464",
+  margin: "0",
+};
+
+export const InputStyle = {
+  fontFamily: "Inter",
+  border: "1px solid #D0D5DD",
+  borderRadius: "8px",
+  margin: "4px 0 8px 0",
+  ".MuiInputBase-input": {
+    padding: "10px 12px",
+  },
+  ".MuiButtonBase-root": {
+    margin: '0px',
+  }
+};
+
+export const CustomButtonStyle = {
+  padding: "0",
+  margin: "16px 0",
+  height: "44px",
+  textTransform: "none",
+  color: "primary",
+  fontSize: "14px",
+  fontWeight: "600",
+};
+
+export const GoogleButtonStyle = {
+  padding: "0",
+  height: "44px",
+  fontFamily: "Inter",
+  border: "1px solid #0241AE",
+  textTransform: "none",
+  fontSize: "14px",
+  fontWeight: "600",
+  marginTop: "16px",
+};
