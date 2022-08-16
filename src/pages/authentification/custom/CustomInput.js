8dad4b3c19d085774d507d6bf44e53a1ce242e62
Ioana Poroch <ioanaroxanaporoch@gmail.com>
2022-08-16 15:23:49 +0300

    Revert Authentification Custom Input

--- src/pages/authentification/custom/CustomInput.js
+++ src/pages/authentification/custom/CustomInput.js
@@ -13,26 +13,21 @@ const CustomInput = ({
   bottomLabel,
   placeholder,
   type,
-  setEnteredValue,
+  setEmail,
   error,
-  id,
-  multiline,
-  sx,
 }) => {
-  console.log("sx", sx);
   return (
     <>
       <InputLabel sx={LabelStyle}>{topLabel}</InputLabel>
       <InputBase
-        onChange={(event) => setEnteredValue(event.target.value)}
-        sx={error.status ? InputStyleError : { ...InputStyle, ...sx }}
-        id={id}
+        onChange={setEmail}
+        sx={error.emailError ? InputStyleError : InputStyle}
+        label={topLabel}
         placeholder={placeholder}
         type={type}
-        multiline={multiline}
       />
-      <InputLabel sx={BottomLabelStyle} error={error.valueError}>
-        {error.valueError ? error.message : bottomLabel}
+      <InputLabel sx={BottomLabelStyle} error={error.emailError}>
+        {error.emailError ? error.message : bottomLabel}
       </InputLabel>
     </>
   );
