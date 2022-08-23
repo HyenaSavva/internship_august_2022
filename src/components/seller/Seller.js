33b6085e0646f5b362fda2fc02cb14ef693d1687
Ioana Poroch <ioanaroxanaporoch@gmail.com>
2022-08-23 12:07:07 +0300

    seller infos from local storage

--- src/components/seller/Seller.js
+++ src/components/seller/Seller.js
@@ -1,23 +1,24 @@
 import Avatar from "@mui/material/Avatar";
 
 import { Text } from "UI/text/Text";
-import avatar from "assets/images/avatar.png";
 
 import SellerStyle, { sellerName, sellerAvatar } from "./SellerStyle";
 import { appTheme } from "themes/themes";
 
 export const Seller = ({ className }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
   return (
     <div className={`seller ${className}`}>
-      <Avatar alt="Seller name" src={avatar} sx={sellerAvatar} />
+      <Avatar alt="Seller name" src={user?.photo} sx={sellerAvatar} />
 
       <div className="seller__infos">
         <Text variant="body2" sx={sellerName}>
-          Jordan Henderson
+          {user?.fullName}
         </Text>
 
         <Text variant="body2" color={appTheme.palette.secondary.light}>
-          Joined in July 2014
+          Joined in July 2022
         </Text>
 
         <Text variant="body2" color={appTheme.palette.secondary.light}>
